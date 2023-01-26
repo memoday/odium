1a57597bf6e97a421dea70bd37cff975fc088027
Yoon <74040890+memoday@users.noreply.github.com>
2023-01-26 16:14:37 +0900

    toggle Update
    심볼 토글 부자연스러운 연출 개선

--- mouseover.js
+++ mouseover.js
@@ -1,16 +1,22 @@
 let odium = document.getElementById("symbol1");
 let logStyle = document.getElementById("log").style;
+let levelStyle = document.getElementById("level").style;
 let log = document.getElementById("log");
+let level = document.getElementById("level");
 
-// odium.addEventListener("click", function (event) {
-//   logStyle.display = "block";
-//   logStyle.animation = "fadein .3s";
-// }, false);
+odium.addEventListener("mouseover", function (event) {
+  logStyle.display = "block";
+  logStyle.animation = "fadein .3s";
+  levelStyle.display = "block";
+  levelStyle.animation = "fadein .3s";
+}, false);
 
-// odium.addEventListener("mouseout", function (event) {
-//     logStyle.display = "none";
-// }, false); 
+odium.addEventListener("mouseout", function (event) {
+    logStyle.display = "none";
+    levelStyle.display = "none";
+}, false); 
 
 odium.addEventListener("click", function (event) {
   log.classList.toggle('active');
+  level.classList.toggle('active');
 }, false);
