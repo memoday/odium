4da9882c6d21f65c8376a55fe20ed8e688d85c31
Yoon <74040890+memoday@users.noreply.github.com>
2023-01-26 10:24:47 +0900

    Click Update
    Hover -> Click으로 변경

--- mouseover.js
+++ mouseover.js
@@ -1,11 +1,16 @@
 let odium = document.getElementById("symbol1");
 let logStyle = document.getElementById("log").style;
+let log = document.getElementById("log");
 
-odium.addEventListener("mouseover", function (event) {
-  logStyle.display = "block";
-  logStyle.animation = "fadein .3s";
+// odium.addEventListener("click", function (event) {
+//   logStyle.display = "block";
+//   logStyle.animation = "fadein .3s";
+// }, false);
+
+// odium.addEventListener("mouseout", function (event) {
+//     logStyle.display = "none";
+// }, false); 
+
+odium.addEventListener("click", function (event) {
+  log.classList.toggle('active');
 }, false);
-
-odium.addEventListener("mouseout", function (event) {
-    logStyle.display = "none";
-}, false); 
