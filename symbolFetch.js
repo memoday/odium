86e5a38cd0d3254f52aecde397bcf671bd93d1a0
Yoon <74040890+memoday@users.noreply.github.com>
2023-06-13 13:55:08 +0900

    Update

---
+++ symbolFetch.js
@@ -0,0 +1,14 @@
+fetch("https://port-0-odium-fastapi-6g2llfhttxjl.sel3.cloudtype.app/")
+  .then((response) => response.json())
+  .then((data) => {
+    const odiumData = data.odium;
+    const shangrilaData = data.shangrila;
+    const currentValue = `${odiumData.currentValue}/${odiumData.currentLevelMaxValue}`;
+    document.getElementById(
+      "currentLevel"
+    ).innerHTML = `Lv.${odiumData.currentLevel}`;
+    document.getElementById("nowValue").innerHTML = currentValue;
+  })
+  .catch((error) => {
+    console.error("Error occurred while fetching data:");
+  });
